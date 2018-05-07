759f43e6bab471a5b608c30363c4a5f58342b69c
Brunosccp <rocca_bruno@hotmail.com>
2018-05-07 16:44:37 -0300

    commit inicial

---
+++ FifaManagerPlayerFinder/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FifaManagerPlayerFinder
+//
+//  Created by Bruno Rocca on 07/05/2018.
+//  Copyright © 2018 Bruno Rocca. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
